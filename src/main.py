dce88be866c68e805576c7c883c7a4f406e88ef4
Dukeyes <daniel.dik@hotmail.com>
2023-03-14 15:35:05 -0300

    Code updated without slice and phylogenetic tree

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
-from slice import Slice
-from align import Align
 from mutate import Mutate
-
 
 if __name__ == "__main__":
 
-    pass
-    # mutate = Mutate("Human_FVIII_prot.fasta", "FVIII_point_mutations_v1_clean.csv")
-
-    # aligne = Align()
-
-    # slice = Slice()
+    mutate = Mutate(
+        input_wild="Human_FVIII_prot.fasta",
+        input_champ="champ-mutation-list-q4-clean.xlsx",
+        folder="datasets",
+    )
--- src/mutate.py
+++ src/mutate.py
@@ -1,80 +1,104 @@
-import pandas as pd
-from Bio import SeqIO
-from Bio.Data.IUPACData import protein_letters_3to1 as prot3to1
-from Bio.Seq import MutableSeq
+import re
 from os.path import abspath, dirname, join
+
+import pandas as pd
+from Bio import AlignIO, SeqIO
+from Bio.Align import MultipleSeqAlignment as MSA
+from Bio.Data.IUPACData import protein_letters_3to1 as prot3to1
+from Bio.Seq import MutableSeq, Seq
+from Bio.SeqRecord import SeqRecord
 from numpy import NaN
+
+
+def get_info(s):
+    wild = re.search("^\D+", s).group()
+    pos = re.search("\d+", s).group()
+    new = re.search("\D+$", s).group()
+    return wild, int(pos), new
 
 
 class Mutate:
     """Class representing the point mutations of the csv file as a dataframe"""
 
-    def __init__(self, input_wild, input_file, folder="datasets", sep="\t"):
+    def __init__(self, input_wild, input_champ, folder):
         """Create a Mutate object"""
 
         # define the file paths
-        input_dir = abspath(join(dirname(__file__), "..", folder))
-        self.input_file = input_file
-        self.input_path = join(input_dir, input_file)
-        self.input_wild_path = join(input_dir, input_wild)
+        dir = abspath(join(dirname(__file__), "..", folder))
+        self.path_champ = join(dir, input_champ)
+        self.path_wild = join(dir, input_wild)
 
-        # create dataframe using csv file
-        self.df = pd.read_csv(self.input_path, sep=sep)
+        # create dataframe using the champ mutation list in excel
+        df = pd.read_excel(self.path_champ)
+
+        # create a dict with severity as key and list of mutations as values
+        self.d = {}
+        for sev, prot in df.groupby("Reported Severity"):
+            self.d[sev] = prot["HGVS Protein"].to_list()
 
         # mutate all sequences in the dataframe
         self.mutate_sequences()
 
-    def mutate_sequences(self):
-        """Apply all mutations of the dataframe in the wild sequence"""
+        # self.df.to_excel("teste.xlsx")
 
-        def mutate_seq(row):
+    def mutate_sequences(self):
+        """Apply all mutations from the champ mutation list"""
+
+        def mutate_seq(prot):
             """Mutate a single sequence"""
 
-            # get the wild sequence and make it mutable
-            wild_seq = MutableSeq(SeqIO.read(self.input_wild_path, "fasta").seq)
+            # get the wild amino acid sequence and make it mutable
+            wild_seq = MutableSeq(SeqIO.read(self.path_wild, "fasta").seq)
 
-            # get alias for used informations
-            index = row.mut_pos - 1
-            wild_aa = row.wild_aa.strip()
-            new_aa = row.new_aa.strip()
+            # alias to faciliate manipulation
+            wild, pos, new = get_info(prot)
 
-            # verify if the wild amino acid is the same as the original
-            can_mutate = prot3to1[wild_aa] == wild_seq[index]
-
-            if not can_mutate:
+            # verify if wild is a valid amino acid
+            if not (wild in prot3to1.keys()):
                 return NaN
+
+            # verify if wild is the same amino acid in the wild sequence to mutate it
+            if not (prot3to1[wild] == wild_seq[pos - 1]):
+                return NaN
+
+            # * is stop codon
+            if new == "*":
+                wild_seq[pos - 1 :] = ""
             else:
-                # missense just change one amino acid in a specific position
-                if row.effect == "Missense":
-                    wild_seq[index] = prot3to1[new_aa]
+                wild_seq[pos - 1] = prot3to1[new]
 
-                # nonsense add a stop codon that eliminate the rest of the sequence
-                elif row.effect == "Nonsense":
-                    wild_seq[index:] = ""
+            return str(wild_seq)
 
-                return str(wild_seq)
+        seq_record = SeqIO.read(self.path_wild, "fasta")
+        name = seq_record.name
+        description = seq_record.description
 
-        def mutate_id(row):
-            """Create an id for the mutated sequence"""
-            new_aa = row.new_aa
-            wild_aa = row.wild_aa
-            mut_pos = row.mut_pos
-            sev = row.severity[:3]
-            return f"{sev}_p{{{wild_aa}{mut_pos}{new_aa}}}"
+        seq_records = []
+        for sev, prots in self.d.items():
+            for prot in prots:
+                id = sev + "{" + prot + "}"
 
-        # alias to improve readability
-        df = self.df
-
-        # create an id for each mutated sequence
-        df["mutated_id"] = df.apply(mutate_id, axis=1)
-
-        # apply the mutation on each sequence
-        df["mutated_seq"] = df.apply(mutate_seq, axis=1)
-
-        # drop rows that could not mutate
-        print(f"Could not mutate {df.isna().sum().sum()} sequences.")
-        df.dropna(inplace=True)
+                seq = mutate_seq(prot)
+                if seq is NaN:
+                    continue
+                else:
+                    seq_record = SeqRecord(MutableSeq(seq), id, name, description)
+                    seq_records.append(seq_record)
 
         # pad all mutated sequences
-        max_len = max(len(s) for s in df.mutated_seq.values)
-        df["mutated_seq"] = df.mutated_seq.apply(lambda s: s.ljust(max_len, "x"))
+        max_len = max(len(str(r.seq)) for r in seq_records)
+        print(max_len)
+
+        for r in seq_records:
+            s = str(r.seq).ljust(max_len, "x")
+            r.seq = Seq(s)
+
+        # generate fasta file with mutated sequence records
+        AlignIO.write(MSA(seq_records), f"align_path{len(seq_records)}.fasta", "fasta")
+
+
+mutate = Mutate(
+    input_wild="Human_FVIII_prot.fasta",
+    input_champ="champ-mutation-list-q4-clean.xlsx",
+    folder="datasets",
+)
